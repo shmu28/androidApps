3ac85e588bc84666d784d2408a3ddaccbf3049d9
shmu28 <shmu28@abv.bg>
2019-03-17 22:12:24 +0000

    1st commit

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            button.Clicked += Button_ClickedAsync;
+        }
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            string name = entry.Text;
+            await DisplayAlert("Hi there", $"Hello {name}", "OK");
+        }
+    }
+}
